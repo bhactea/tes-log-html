6d73e1a8618c3095cfc01f0f63cd222a0d14fe3d
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 22:34:48 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,39 +1,38 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+  id("com.android.application")
+  kotlin("android")
 }
 
 android {
-    namespace = "com.harpa.logger"
-    compileSdk = 33
+  namespace       = "com.harpa.logger"
+  compileSdk      = 33
 
-    defaultConfig {
-        applicationId = "com.harpa.logger"
-        minSdk = 21
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-    }
+  defaultConfig {
+    applicationId = "com.harpa.logger"
+    minSdk        = 21
+    targetSdk     = 33
+    versionCode   = 1
+    versionName   = "1.0"
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
+  }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
 
-dependencies {
-    compileOnly("de.robv.android.xposed:api:82")
+  // Jika module.prop di root proyek:
+  sourceSets["main"].assets.srcDir(rootProject.file("module.prop").parent)
 }
