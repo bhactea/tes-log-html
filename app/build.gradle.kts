1ee5dc9fb4d15c94cb11c3552d847a2d532e0fa7
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 15:00:22 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,27 @@
 plugins {
-  id("com.android.library")
-  kotlin("android")
+    id("com.android.library")
+    kotlin("android")
 }
 
 android {
-  compileSdk = 33
-  defaultConfig {
-    applicationId = "com.harpa.logger"
-    minSdk = 21
-    targetSdk = 33
-    versionCode = 1
-    versionName = "1.0"
-  }
-  buildToolsVersion = "34.0.0"
+    namespace = "com.harpa.logger"    // ganti sesuai package di module.prop
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+        // jangan ada applicationId, versionCode, versionName di library
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
-  implementation("de.robv.android.xposed:api:82")
+    implementation("de.robv.android.xposed:api:82")
 }
