e6d0375ada0cfe48191e37de07d0748756da1421
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 23:15:16 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-  id("com.android.library") version "8.1.0" apply false
-  kotlin("android") version "1.9.0" apply false
+    id("com.android.application") version "8.1.0" apply false
+    kotlin("android") version "1.9.0" apply false
 }
-
-// Jangan ada blok repositories di sini
