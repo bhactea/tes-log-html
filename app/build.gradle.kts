8fc78f4c60671a01580c294424f661464563a870
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 15:37:04 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,26 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
+  id("com.android.library")
+  kotlin("android")
 }
 
 android {
-    namespace = "com.harpa.logger"
-    compileSdk = 33
+  namespace = "com.harpa.logger"
+  compileSdk = 33
 
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
+  defaultConfig {
+    minSdk = 21
+    targetSdk = 33
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
 }
 
 dependencies {
-    // Xposed API sebagai compileOnly (tidak di-include di APK final)
-    compileOnly("de.robv.android.xposed:api:82")
+  compileOnly("de.robv.android.xposed:api:82")
 }
