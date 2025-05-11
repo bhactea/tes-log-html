553c3d46a8358ea12015b1a879d7427f46e89936
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 15:34:44 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
-// Root-level Gradle settings: daftarkan plugin Android & Kotlin tapi jangan auto-apply
 plugins {
-  // Android Gradle Plugin
+  // tidak auto-apply, hanya mendeklarasikan
   id("com.android.library") version "8.1.0" apply false
-  // Kotlin Android Plugin
   kotlin("android") version "1.9.0" apply false
 }
 
-// (Opsional) konfigurasi buildscript atau repositori tambahan
+allprojects {
+  repositories {
+    google()
+    mavenCentral()
+    // Xposed API
+    maven { url = uri("https://api.xposed.info/") }
+  }
+}
