572b7712760a8fe4a7031be03c56bb2a71312209
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 14:55:22 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
+// Root-level Gradle settings: daftarkan plugin Android & Kotlin tapi jangan auto-apply
 plugins {
-  id("com.android.library") version "8.1.0"
-  kotlin("android") version "1.9.0"
+  // Android Gradle Plugin
+  id("com.android.library") version "8.1.0" apply false
+  // Kotlin Android Plugin
+  kotlin("android") version "1.9.0" apply false
 }
+
+// (Opsional) konfigurasi buildscript atau repositori tambahan
