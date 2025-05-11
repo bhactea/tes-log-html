9c8555ea133035dab81e91f6f4872accdb2c3833
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 16:27:54 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,6 @@
 plugins {
-  // tidak auto-apply, hanya mendeklarasikan
   id("com.android.library") version "8.1.0" apply false
   kotlin("android") version "1.9.0" apply false
 }
 
-allprojects {
-  repositories {
-    google()
-    mavenCentral()
-    // Xposed API
-    maven { url = uri("https://api.xposed.info/") }
-  }
-}
+// Jangan ada blok repositories di sini
