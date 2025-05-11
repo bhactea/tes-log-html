9836bd993ccaea36eb03c1712dd7ba38fde58e02
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-11 23:03:23 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,36 +4,10 @@ plugins {
 }
 
 android {
-    namespace       = "com.harpa.logger"
-    compileSdk      = 33
-
     defaultConfig {
         applicationId = "com.harpa.logger"
-        minSdk        = 21
-        targetSdk     = 33
         versionCode   = 1
         versionName   = "1.0"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    compileOnly("de.robv.android.xposed:api:82")
+    // … (compileSdk, buildTypes, dll.)
 }
